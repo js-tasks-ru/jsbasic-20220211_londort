7e25b16960c9f2ef759f35b73a6387242d7cb247
Londort <landort@gmail.com>
2022-02-16 18:08:18 +0100

    UpperCase first symbol and an empty line

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,10 @@
-function ucFirst(str) {
-  // ваш код...
+function ucFirst (str) {
+  if (str == false) {
+    return str
+  }
+  let trimmer = str.trim();
+  let upperCase = trimmer[0].toUpperCase();
+  return upperCase + trimmer.slice(1);
 }
+
+ucFirst(``);
