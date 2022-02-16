4130aefabdf924ed65e7a9aa94e236b57768c3fd
Londort <landort@gmail.com>
2022-02-16 13:49:08 +0100

    factorial calculation

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
-function factorial(n) {
-  // ваш код...
+function factorial(n){
+  let x = 1;
+  while(n) {
+    x *= n--;
+  }
+  return x;
 }
+
+factorial (0)
+factorial (1)
+factorial (3)
+factorial (5)
