164b1e996d568c9db4a75b7abd35d614481d1080
Londort <landort@gmail.com>
2022-02-17 16:01:17 +0100

    name validation for sayHello

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name == null || name.includes(' ') || name.length < 4) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function sayHello() {
