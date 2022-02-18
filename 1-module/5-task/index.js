da1560c84249fc623122ab2e20f3b2de977945c8
Londort <landort@gmail.com>
2022-02-18 11:01:21 +0100

    Truncate function

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  let length = str.length;
+  if (length <= maxlength) {
+    return str;
+  } else {
+    return str.slice(0, maxlength - 1) + '...'
+  }
 }
+
+truncate ('Вот, что мне хотелось бы сказать на эту тему:', 20)
+truncate ('Всем привет!', 20)
