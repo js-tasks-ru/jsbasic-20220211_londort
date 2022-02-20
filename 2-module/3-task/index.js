4dfd2d3645396630e15a4b4ee7cbf62b98f59802
Londort <landort@gmail.com>
2022-02-20 02:25:44 +0100

    2-module/3-task - calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,17 @@
 let calculator = {
-  // ваш код
+  read (a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  }
 };
-
+calculator.read(3, 5);
+console.log(calculator.sum());
+console.log(calculator.mul());
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
