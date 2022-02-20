e5889f1b70b313f49b7e70b829a1eba94ea55b0c
Londort <landort@gmail.com>
2022-02-20 21:32:53 +0100

    2-module/1-task: Salary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,22 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+
+let sum = 0;
+
 function sumSalary(salaries) {
-  // ваш код...
-}
+  for (prop in salaries) {
+		a = typeof salaries[prop]; // вычислили тип данных свойств
+  	if (a === 'number' && (!NaN || !Infinity || !-Infinity)) { //убрали ненужные типы
+    	sum += salaries[prop]; // сложили между собой 
+  	};
+	};
+  return sum;
+};
+
+sumSalary(salaries);
