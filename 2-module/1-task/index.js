b4d27b8cb4f128ca73a2c3112aeb78cba4054c77
Londort <landort@gmail.com>
2022-02-21 12:53:03 +0100

    убрал комментарии из тела кода

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -11,9 +11,9 @@ let sum = 0;
 
 function sumSalary(salaries) {
   for (prop in salaries) {
-		a = typeof salaries[prop]; // вычислили тип данных свойств
-  	if (a === 'number' && (!NaN || !Infinity || !-Infinity)) { //убрали ненужные типы
-    	sum += salaries[prop]; // сложили между собой 
+		a = typeof salaries[prop];
+  	if (a === 'number' && (!NaN || !Infinity || !-Infinity)) {
+    	sum += salaries[prop];
   	};
 	};
   return sum;
