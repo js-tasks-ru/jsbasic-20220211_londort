afc83bf2b8d3890e991fa8fc019c090d6bbcd2a4
Londort <landort@gmail.com>
2022-03-11 23:24:45 +0100

    3-module-5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let a = str.split(' ')
+  let b = a.filter(function(elem) {
+    return Number(elem);
+  })
+  let c = b.map(val => +val)
+  let max = Math.max.apply(null, c)
+  let min = Math.min.apply(null, c)
+  let result = {
+    min: min,
+    max: max
+  };
+  return result
 }
