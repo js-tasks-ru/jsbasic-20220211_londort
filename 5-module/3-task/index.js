06b07eef1ff019b4c495caa8000c76ece6d09cd4
Londort <landort@gmail.com>
2022-03-18 13:01:07 +0100

    5-module 1,2,3 tasks

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  const sliderLine = document.querySelector('.carousel__inner'),
+        slides = document.querySelectorAll('.carousel__slide'),
+        prev = document.querySelector('.carousel__arrow_left'),
+        next = document.querySelector('.carousel__arrow_right');
+
+  let slideWidth = slides[0].offsetWidth,
+      offset = 0;
+
+  prev.style.display = 'none';
+
+  next.addEventListener('click', () => {
+    offset = offset + slideWidth;
+    sliderLine.style.left = -offset + 'px';
+
+    if (offset == slideWidth * (slides.length - 1)) {
+      next.style.display = 'none';
+    }else {
+      next.style.display = '';
+    }
+  });
+
+  prev.addEventListener('click', () => {
+    offset = offset - slideWidth;
+    if (offset !== 0) {
+      prev.style.display = '';
+    };
+    sliderLine.style.left = -offset + 'px';
+  }); 
 }
