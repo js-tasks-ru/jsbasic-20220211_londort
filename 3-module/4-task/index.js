5fb499cd63212265ff2622fb5849f6af1b59265e
Londort <landort@gmail.com>
2022-03-20 13:27:48 +0100

    3-module-4-task undone

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,27 @@
 function showSalary(users, age) {
-  // ваш код...
+  let x = users.filter(item => item.age <= age)
+  let y = '';
+  x.map (function(el, index){
+    if (index == x.length - 1) {
+      y += `${el.name} ${el.balance}`;
+    } else {
+      y += `${el.name} ${el.balance}\n`;
+    }
+  }).join('')
+ 	return y;
 }
+
+let user1 = {
+  "balance": "$1,825.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 21,
+  "name": "Golden Branch",
+  "gender": "male",
+  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+let users = [user1, ...rest];
+
+let result = showSalary(users, 40);
+result
