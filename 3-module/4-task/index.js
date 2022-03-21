a15ca6a560ce31ff34c9c3c3e3cfc644b9289ddb
Londort <landort@gmail.com>
2022-03-21 13:00:04 +0100

    3-module-4-task complete

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,15 +1,11 @@
 function showSalary(users, age) {
-  let x = users.filter(item => item.age <= age)
-  let y = '';
-  x.map (function(el, index){
-    if (index == x.length - 1) {
-      y += `${el.name} ${el.balance}`;
-    } else {
-      y += `${el.name} ${el.balance}\n`;
-    }
-  }).join('')
+  let y = users.filter(item => item.age <= age)
+               .map (function(el){
+                  return el.name + ', ' + el.balance;
+               })
+               .join('\n');
  	return y;
-}
+};
 
 let user1 = {
   "balance": "$1,825.65",
