2c55b6660050c8fd394d91f682cf1ca7724bd540
Londort <landort@gmail.com>
2022-03-24 11:26:24 +0100

    3-module-4-task complete

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -6,18 +6,3 @@ function showSalary(users, age) {
                .join('\n');
  	return y;
 };
-
-let user1 = {
-  "balance": "$1,825.65",
-  "picture": "https://placehold.it/32x32",
-  "age": 21,
-  "name": "Golden Branch",
-  "gender": "male",
-  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-};
-
-let users = [user1, ...rest];
-
-let result = showSalary(users, 40);
-result
